fb3941189728ba2a5c4a31d88076dd2297f7ccd3
sa6o sa6o <aleksandarboev95@gmail.com>
2019-05-28 10:20:30 +0300

    Added solution. Used function as an object initialized class

--- 04.objects-and-dom-exercise/tasks/t09_order_the_names/solution.js
+++ 04.objects-and-dom-exercise/tasks/t09_order_the_names/solution.js
@@ -1,4 +1,43 @@
 function solve() {
+    const functionObject = (liElement) => {
+        const savedLiElement = liElement;
+        return {
+            appendName: (name) => {
+                if (savedLiElement.textContent === '') {
+                    savedLiElement.textContent = name;
+                } else {
+                    savedLiElement.textContent += ', ' + name;
+                }
+            },
+        }
+    };
 
-    //TODO...
+    const allLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    const liElements = document.getElementsByTagName('li');
+
+    const letterListItemObject = [];
+
+    for (let i = 0; i < liElements.length; i += 1) {
+        letterListItemObject[allLetters[i]] = functionObject(liElements[i]);
+    }
+
+    const inputFieldElement = document.getElementsByTagName('input')[0];
+    const addButtonElement = document.getElementsByTagName('button')[0];
+    console.log(addButtonElement);
+
+    addButtonElement.addEventListener('click', () => {
+            const nameToBeAdded = inputFieldElement.value;
+            if (!nameToBeAdded)  //if it's an empty string, do nothing
+                return;
+
+            const firstLetter = '' + nameToBeAdded.charAt(0).toLowerCase();
+
+            if (letterListItemObject[firstLetter] === undefined)
+                return;
+
+            letterListItemObject[firstLetter].appendName(nameToBeAdded);
+        }
+    )
 }
+
