9aad7addc98c3a4fcd455f4212f75daa5225e10b
sa6o sa6o <aleksandarboev95@gmail.com>
2019-05-28 10:19:16 +0300

    Simplified solution and fixed minor bug

--- 04.objects-and-dom-exercise/tasks/t04_javascript_quizz/solution.js
+++ 04.objects-and-dom-exercise/tasks/t04_javascript_quizz/solution.js
@@ -1,43 +1,38 @@
-function solve() { //TODO task description does not specify how exactly to visualize result. Works properly as it is tho
+function solve() {
     const addHidden = sectionElement => sectionElement.style.display = 'none';
     const removeHidden = sectionElement => sectionElement.style.display = 'block';
+
     const rightAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
     let rightAnswersCounter = 0;
 
-    const resultHeader = document.createElement('h1');
-    const quizDivElement = document.getElementById('quizzie').appendChild(document.createElement('h1'));
-    // const visualizeResult = string => {
-    //     const resultLiElement = document.getElementsByClassName('results-inner')[0];
-    //     const resultHeader = resultLiElement.children[0];
-    //     resultHeader.textContent = string;
-    //     resultLiElement.style.display = 'block';
-    // };
+    const visualizeResult = quizResultMessage => {
+        const resultUlElement = document.getElementById('results');
+        const resultHeaderElement = resultUlElement.getElementsByTagName('h1')[0];
+        resultUlElement.style.display = 'block';
+        resultHeaderElement.textContent = quizResultMessage;
+    };
 
     const sections = document.getElementsByTagName('section');
-    const answerDivBoxes = document.getElementsByClassName('answer-wrap');
 
-    for (let i = 0; i < answerDivBoxes.length; i += 1) {
-        answerDivBoxes[i].addEventListener('click', () => {
-            const selectedAnswerParagraphElement = answerDivBoxes[i].children[0];
-            const selectedAnswerValue = selectedAnswerParagraphElement.textContent;
+    for (let i = 0; i < sections.length; i += 1) {
+        const answerDivBoxes = Array.from(sections[i].getElementsByClassName('answer-wrap'));
 
-            if (rightAnswers.includes(selectedAnswerValue)) { //if selected answer is right
-                rightAnswersCounter++;
-                if (rightAnswersCounter === sections.length) { //if all answers are correct
-                    sections[rightAnswersCounter - 1].style.display = 'none';
-                    resultHeader.textContent = 'You are recognized as top JavaScript fan!';
-                    quizDivElement.appendChild(resultHeader);
-                    // visualizeResult('You are recognized as top JavaScript fan!');
+        for (let currentAnswerDivBox of answerDivBoxes) {
+            currentAnswerDivBox.addEventListener('click', () => {
+                addHidden(sections[i]);
+
+                const answerValue = currentAnswerDivBox.getElementsByClassName('answer-text')[0].textContent;
+
+                if (answerValue === rightAnswers[i]) {
+                    rightAnswersCounter++;
+                    if (rightAnswersCounter === sections.length)
+                        visualizeResult('You are recognized as top JavaScript fan!');
+                     else
+                        removeHidden(sections[i + 1]);
                 } else {
-                    addHidden(sections[rightAnswersCounter - 1]);
-                    removeHidden(sections[rightAnswersCounter]);
+                    visualizeResult(`You have ${rightAnswersCounter} right answers`)
                 }
-            } else { //if selected answer is wrong
-                addHidden(sections[rightAnswersCounter]);
-                resultHeader.textContent = `You have ${rightAnswersCounter} right answers`;
-                quizDivElement.appendChild(resultHeader);
-                // visualizeResult(`You have ${rightAnswersCounter} right answers`);
-            }
-        })
+            });
+        }
     }
 }
